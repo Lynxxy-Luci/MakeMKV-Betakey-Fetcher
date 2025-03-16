b6df0451f48a676dfee8051243789d9df64a8da7
Karla Lucia Lin <165141827+Lynxxy-Luci@users.noreply.github.com>
2025-03-16 17:24:40 +0100

    Create makemkv_keyfetch.sh

---
+++ makemkv_keyfetch.sh
@@ -0,0 +1,19 @@
+# inspired by: https://pastebin.com/vafpYkfa
+# and: https://wiki.indie-it.com/wiki/MakeMKV
+# 
+# add to crontab for weekly execution:
+# @weekly /path/to/makemkv_keyfetch.sh
+
+betakey=$(lynx -dump -nonumbers -nolist "http://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053" | awk '/Code: Select all/{getlin>
+if [ -n "${betakey}" ]; then
+        echo ""
+        echo "Current MakeMKV Beta Key: ${betakey}"
+        cd ~/.MakeMKV/
+        sed -i "s/^app_Key=.*/app_Key=${betakey}/" settings.conf
+        echo "----------------------------------------------------------------------------------------------"
+        echo "Key successfully updated."
+        echo ""
+else
+        echo "Could not find BETAKEY."
+fi
+
